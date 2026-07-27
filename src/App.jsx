dca87bb51f6c387acc8892aa7f5c9d85b3773e9a
Chandrajit06 <chandrajit720@gmail.com>
2026-07-27 15:01:45 +0530

    create router

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,18 @@ function App() {
 
           <div>
             <div className="bg-slate-800 mt-5 flex justify-between py-3 px-4 rounded-lg shadow-2xl shadow-black/40">
-              <span className="text-xl">
-                Password Generator</span>
-              <button 
+              <span className="text-xl">Counter</span>
+              <Link    to="/counter"
                 className="p-1 bg-blue-600 hover:bg-blue-700 cursor-pointer rounded-lg px-2 shadow-2xl shadow-black/40"
               >
-                Go to Website</button>
+              Go to Website</Link>
             </div>
-            
             <div className="bg-slate-800 mt-5 flex justify-between py-3 px-4 rounded-lg shadow-2xl shadow-black/40">
-              <span className="text-xl">
-                Password Generator</span>
-              <button className="p-1 bg-blue-600 hover:bg-blue-700 cursor-pointer rounded-lg px-2 shadow-2xl shadow-black/40"
-                
+              <span className="text-xl">Background Changer</span>
+              <Link    to="/bgchanger"
+                className="p-1 bg-blue-600 hover:bg-blue-700 cursor-pointer rounded-lg px-2 shadow-2xl shadow-black/40"
               >
-                Go to Website</button>
+              Go to Website</Link>
             </div>
             
           </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-// import router from './router.jsx'
+import router from './router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <RouterProvider router={router} /> */}
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
