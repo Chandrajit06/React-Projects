9965fa6e6e9edd3bcd4a212b16caa8b16d10d578
Chandrajit06 <chandrajit720@gmail.com>
2026-07-29 15:24:06 +0530

    add bgchanger

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import App from "./App";
 import Counter from "./components/Counter/Counter.jsx";
+import BgChanger from "./components/BgChanger/BgChanger.jsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route>
             <Route path="/" element={<App />} />
             <Route path="/counter" element={<Counter />} />
+            <Route path="/bgchanger" element={<BgChanger />} />
         </Route>
     )
 )
