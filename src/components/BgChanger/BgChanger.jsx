ab619e412298662f3f0f60f729417e464f98979b
Chandrajit06 <chandrajit720@gmail.com>
2026-07-29 15:28:33 +0530

    add Link to home page

--- src/components/BgChanger/BgChanger.jsx
+++ src/components/BgChanger/BgChanger.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function BgChanger() {
-    const [color, setColor] = useState("pink")
+    const [color, setColor] = useState("blue")
 
   return (
     <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
-        <div className="fixed flex flex-wrap justify-center bottom-40 inset-x-0 px-2">
+        <div className="fixed flex flex-wrap justify-center bottom-35 inset-x-0 px-2">
             <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
                 <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "red"}}  onClick={() => setColor("red")}>Red</button>
                 <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "green"}}  onClick={() => setColor("green")}>Green</button>
@@ -16,6 +17,10 @@ function BgChanger() {
                 <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "black"}}  onClick={() => setColor("black")}>Black</button>
             </div>
         </div>
+        <Link to="/" 
+            className="bg-slate-800 text-xl text-white px-2 py-1 rounded-lg absolute bottom-12 left-1/2 -translate-x-1/2 shadow-2xl shadow-black/40 hover:bg-slate-700">
+        ← Back to Projects
+        </Link>
     </div>
   )
 }
